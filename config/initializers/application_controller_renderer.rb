d3646f14fee357dfe027bf1ccb683122c977a345
Ubuntu <ubuntu@ip-172-31-29-33.us-east-2.compute.internal>
2021-05-01 07:35:10 +0000

    ユーザー周りの実装完了

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '1ac50acf9a1a4d8f6ae615947c2445ba622a0b1e23e631a8efbf139dc36d3ad2896b5e8a8758491e0a99f7a7273c748141df4dd055120e71d3b0b29672165458'
