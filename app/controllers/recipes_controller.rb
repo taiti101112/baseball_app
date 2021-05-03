33433b91b0208e0b97734444a47e1ec04a574102
Ubuntu <ubuntu@ip-172-31-29-33.us-east-2.compute.internal>
2021-05-03 06:43:01 +0000

    編集機能実装完了

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -18,7 +18,16 @@ class RecipesController < ApplicationController
   end 
 
   def edit
+    @recipe = Recipe.find(params[:id])
   end
+  
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(recipe_params)
+    redirect_to recipe_path(@recipe)
+  end 
+  
+  
   
   private
   def recipe_params
